90a4ead36c76ddd30c03c66226b7c5e6be5c79a3
rhanilham <rhanilhamfadlillatulr@gmail.com>
2024-03-04 00:15:12 +0700

    eloquent orm

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,8 +9,8 @@ class LevelController extends Controller
 {
     public function index()
     {
-        // DB::insert('insert into m_level(level_code, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-        // return 'Insert data baru berhasil';
+        DB::insert('insert into m_level(level_code, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        return 'Insert data baru berhasil';
 
         // $row = DB::update('update m_level set level_nama = ? where level_code = ?', ['Customer', 'CUS']);
         // return 'Update data berhasil. Jumlah data yang diupdate : ' . $row . 'baris';
@@ -18,7 +18,7 @@ class LevelController extends Controller
         // $row = DB::delete('delete from m_level where level_code = ? ', ['CUS']);
         // return 'Delete data berhasil. Jumlah data yang dihapus : ' . $row . 'baris';
 
-        $data = DB::select('select * from m_level');
-        return view('level', ['data' => $data]);
+        // $data = DB::select('select * from m_level');
+        // return view('level', ['data' => $data]);
     }
 }
