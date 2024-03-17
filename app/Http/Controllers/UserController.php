c0f37a347369117a57825b81cf9f4fcbd533ff1e
rhanilham <rhanilhamfadlillatulr@gmail.com>
2024-03-17 21:54:43 +0700

    Praktikum 2.7 – Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index () 
+    public function index()
     {
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);          
+        $user = UserModel::with('level')->get(); return view('user', ['data' => $user]);
     }
     
     public function tambah()
